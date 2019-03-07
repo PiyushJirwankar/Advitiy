619ccef4cbd3a3c10a94571d8288b9e74d396ad6
PiyushJirwankar <37498286+PiyushJirwankar@users.noreply.github.com>
2019-03-07 23:59:39 +0530

    Update lr_pwm.py

--- lr_pwm.py
+++ lr_pwm.py
@@ -3,24 +3,31 @@ import matplotlib.pyplot as plt
 import square
 
 R = 5
-L = 5
+L = 5000
 V_p = 5
 len = 1000
 duty = 0.5
-timeP = 1
+timeP = 10
 
-t=np.linspace(0,10,len)
+t_start = 0
+t_end = 10
+
+t=np.linspace(t_start,t_end,len)
+h=int((t_end-t_start)/(len))
+
 V=V_p*square.square(t, duty, timeP, len)
-output_i=np.zeros(len)
-#plt.plot(t,V)
-for i in range(1000):
+
+i_out = np.zeros(len)
+
+for i in range(1,len):
     #V[0]=5
-    if t[i]%timeP<=duty*timeP:
-        V[i]=(V[i])*(1-np.exp(-R*t[i]/L))
-        output_i[i]=V[i]/R
-    else:
-        V[i]=V[i-1]*(np.exp(-R*t[i]/L))
-        output_i[i]=V[i]/R
-plt.plot(t, output_i)
-#plt.ylim(0,1)
-#plt.xlim(1.4,1.6)
+    if t[i]%timeP<=duty*timeP:                            #'PWM volatage starts with high'
+        i_out[i] = (V_p - (V_p-i_out[i-1]*R)*(np.exp(-(t[i]-t_start)*R/L)))/R
+    #    i_out[i]=V_p*(1-np.exp(-t[i]*R/L))/R + i_out[i-1]*(np.exp(-t[i]*R/L))
+    #else:
+    #    V[i]=V[i-1]
+    #    V[i]=V[i-1]*(np.exp(-R*t[i]/L))
+    #    output_i[i]=V[i]/R
+plt.plot(t, i_out)
+#plt.ylim(0,0.05)
+#plt.xlim(0,0.5)
